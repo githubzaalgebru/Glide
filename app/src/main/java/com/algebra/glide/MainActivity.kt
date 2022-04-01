b631ef0c2719d58b8140310bb1d48708faff886b
office <bla@bla.com>
2022-04-01 19:42:21 +0200

    Slikice

--- app/src/main/java/com/algebra/glide/MainActivity.kt
+++ app/src/main/java/com/algebra/glide/MainActivity.kt
@@ -2,6 +2,7 @@ package com.algebra.glide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +15,9 @@ class MainActivity : AppCompatActivity() {
 
         bGenerate.setOnClickListener {
             val url = photos.randomImage( )
+            Glide.with( this )
+                .load( url )
+                .into( imageView )
         }
     }
 }
